beb35e55818c543df364ad9a9a8bc6ee6e4c6dd1
OscarEreu <est.oscar.jherrera@unimilitar.edu.co>
2026-04-16 00:28:20 -0500

    Puzzle V4

--- Alastor/Assets/Scripts/Puzzle/PuzzleManager.cs
+++ Alastor/Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -2,24 +2,55 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    [Header("Llave correcta del puzzle")]
+    [Header("Llave correcta")]
     public ArrastrarSoltar llaveCorrecta;
+
+    [Header("Temporizador")]
+    public float tiempoRestante = 12f;
+    public float penalizacionError = 2f;
+
+    [Header("UI")]
+    public GameObject panelGameOver;
+
+    private bool puzzleResuelto = false;
+
+    void Update()
+    {
+        if (!puzzleResuelto)
+        {
+            tiempoRestante -= Time.deltaTime;
+
+            if (tiempoRestante <= 0)
+            {
+                tiempoRestante = 0;
+                PerderPuzzle();
+            }
+        }
+    }
 
     public void ValidarLlave(ArrastrarSoltar llaveIntentada)
     {
         if (llaveIntentada == llaveCorrecta)
         {
+            puzzleResuelto = true;
             llaveIntentada.EncajarLlave();
 
-            Debug.Log("Llave correcta. Puzzle resuelto.");
-
-            // Aquí luego puedes abrir puerta
+            Debug.Log("Puzzle resuelto.");
         }
         else
         {
             llaveIntentada.RegresarInicio();
 
-            Debug.Log("Llave incorrecta.");
+            tiempoRestante -= penalizacionError;
+
+            Debug.Log("Llave incorrecta. -2 segundos.");
         }
+    }
+
+    void PerderPuzzle()
+    {
+        Debug.Log("Tiempo agotado. Te atraparon.");
+
+        panelGameOver.SetActive(true);
     }
 }
